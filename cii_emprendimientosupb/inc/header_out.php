16f0604bb73ea38510ce9ecd090097a7a9553433
mrAntonio10 <111607077+mrAntonio10@users.noreply.github.com>
2022-10-23 22:20:49 -0400

    Update header_out.php

--- cii_emprendimientosupb/inc/header_out.php
+++ cii_emprendimientosupb/inc/header_out.php
@@ -2,13 +2,13 @@
     <a href="index.php"><img src="resources/img/emprendimientosupb_logo.png" alt="EmprendimientosUPB" class="img-logo"></a>
     <div class="menu">
         <a href="inicio_sesion.php" title="Iniciar sesión">
-            <i class="fa-solid fa-right-to-bracket"></i>  
+            <em class="fa-solid fa-right-to-bracket"></em>  
         </a>
         <a href="emprendimiento.php?emprendimiento=id" title="Mi emprendimiento">
-            <i class="fa-solid fa-user"></i> 
+            <em class="fa-solid fa-user"></em> 
         </a>
         <a href="cerrar_sesion.php" title="Cerrar sesión">
-            <i class="fa-solid fa-right-from-bracket"></i> 
+            <em class="fa-solid fa-right-from-bracket"></em> 
         </a>
     </div>
 </header>
