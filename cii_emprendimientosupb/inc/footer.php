7be73ea7cecd3fc771de859f34b1807a98beacb6
mrAntonio10 <111607077+mrAntonio10@users.noreply.github.com>
2022-10-23 22:18:59 -0400

    Update footer.php

--- cii_emprendimientosupb/inc/footer.php
+++ cii_emprendimientosupb/inc/footer.php
@@ -1,13 +1,13 @@
 <footer>
         <div>
-        <img src="resources/img/emprendimientosupb_logo.png" title="RPV" width="100px">
+        <img src="resources/img/emprendimientosupb_logo.png" title="RPV" width="100px" alt="logo">
         </div>
         <p class="slogan">¡Emprendimientos UPB la mejor opción para darte a conocer!</p>
         
         <br>
         <h4>EmprendimientosUPB</h4>
         <div>
-            <i class="fa-solid fa-envelope"></i>
+            <em class="fa-solid fa-envelope"></em>
             <a href="mailto:emprendimientos@upb.edu">emprendimientos@upb.edu</a>
         </div>
         <br>    
